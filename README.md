dc3b74cf01ade72e2befef47227c158bfeace541
Kevin Mitchell <shoelessone@gmail.com>
2019-03-03 22:38:06 +0900

    Working towards making the handler a bit more generic

--- README.md
+++ README.md
@@ -4,20 +4,6 @@ This project is meant to contain the serverless setup for the Button Box (workin
 
 This project is heavily based on https://github.com/serverless/examples/tree/master/aws-node-iot-event which is a good place to reference for updates that might be good to pull into this project.
 
-## Useful information
-
-We can check the logs for our functions with
-
-```
-serverless logs --function change-handler
-```
-
-## Functions
-
-### change-detected (`buttonbox-iot-event-processor-change-detected`)
-
-This function will be passed information at the time that a IoT device's shadow is updated. The function will then check the other IoT device shadows that share the same group as the device that triggered the event, and update their shadow as appropriate.
-
 ## Setup
 
 In order to deploy the function simply run
@@ -54,3 +40,21 @@ functions:
 layers:
   None
 ```
+
+### Logs
+
+`serverless logs --function change-handler --tail` will allow you to tail the logs for a particular function (`change-handler` in this example).
+
+So for example to desploy and tail logs, `serverless deploy && serverless logs --function change-handler --tail`
+
+## Functions
+
+### change-detected (`buttonbox-iot-event-processor-change-detected`)
+
+When a change is detected and this function is triggered, the following actions take place:
+
+1. We gat the device ID / name from the trigger event data. This is refered to as `clientid() as clientId` (see: `https://docs.aws.amazon.com/iot/latest/developerguide/iot-sql-functions.html#iot-sql-function-clientid`) in the IoT SQL, but it (for the moment) also is the same value as the "thingName." As of this moment it's not clear to me if this is coincidental or if the two are the same things (todo!).
+2. The thing is queried in order to find out which group(s) the thing is part of
+3. If the thing is part of any groups with a group name following the format "michine\_" then that group is considered the group of interest. Most likely this will always be `michine_michine` but for the time being we're allowing this to be a convention so that in theory we could add additional groups (devices) and keep the same functionality.
+4. This group name is then used to query for all of the devices within it. See `https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Iot.html#listThingsInThingGroup-property`
+5. Each of these Things is updated with the new things value(s).
--- handler.js
+++ handler.js
@@ -4,14 +4,73 @@ let config = {
   thingName: "esp32_14958C"
 };
 var iotdata = new AWS.IotData({ endpoint: config.endpointAddress });
+let iot = new AWS.Iot();
 
-exports.handle = async event => {
-  console.log(event);
-  console.log(event.state.reported);
+exports.handle = async data => {
+  console.log("\n\nevent:");
+  console.log(data);
+
+  let thingGroups = {};
+
+  /***********************************************************
+   *
+   *    Get the groups that this Thing is part of
+   *
+   ***********************************************************/
+  try {
+    thingGroups = await iot
+      .listThingGroupsForThing({
+        thingName: data.clientId
+      })
+      .promise();
+  } catch (e) {
+    console.log(err, err.stack);
+  }
+
+  console.log("listThingGroupsForThing:");
+  console.log(thingGroups);
+
+  /***********************************************************
+   *
+   *    Get the Things in a particular group
+   *
+   ***********************************************************/
+  try {
+    thingGroups = await iot
+      .listThingsInThingGroup({
+        thingGroupName: "michine"
+      })
+      .promise();
+  } catch (err) {
+    console.log(err, err.stack);
+  }
+
+  console.log("listThingsInThingGroup:");
+  console.log(thingGroups);
+
+  /***********************************************************
+   *
+   *    Get the thing shadow for a particular thing
+   *
+   ***********************************************************/
+  let thingShadow = {};
+  try {
+    thingShadow = await iotdata
+      .getThingShadow({ thingName: "esp32_FE0398" })
+      .promise();
+  } catch (e) {
+    console.log(err, err.stack);
+  }
+
+  /***********************************************************
+   *
+   *    Update the thing shadow for a specific Thing
+   *
+   ***********************************************************/
 
   let desiredLedOn = false;
 
-  if (event.state.reported.buttonPressed) {
+  if (data.buttonPressed) {
     desiredLedOn = true;
   }
 
@@ -22,53 +81,20 @@ exports.handle = async event => {
       }
     }
   };
-  console.log(update);
-  console.log("calling updateThing...");
 
-  iotdata.getThingShadow(
-    {
-      thingName: "esp32_FE0398"
-    },
-    function(err, data) {
-      if (err) {
-        console.log(err, err.stack); // an error occurred
-      } else {
-        console.log("------");
-        console.log("------");
-        console.log("------");
-        console.log(data);
-        console.log("------");
-        console.log("------");
-        console.log("------");
-        updateThing(update);
-        console.log("done calling updateThing!");
-      }
-    }
-  );
-
-  function wait() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve("hello"), 1000);
-    });
+  try {
+    let updateThingShadow = await iotdata
+      .updateThingShadow({
+        payload: JSON.stringify(update),
+        thingName: config.thingName
+      })
+      .promise();
+    console.log("updateThingShadowResponse");
+    console.log(updateThingShadow);
+  } catch (e) {
+    console.log("error when calling updateThingShadow");
+    console.log(e);
   }
 
-  console.log(await wait());
-};
-
-let updateThing = function(data) {
-  iotdata.updateThingShadow(
-    {
-      payload: JSON.stringify(data),
-      thingName: config.thingName
-    },
-    function(err, data) {
-      if (err) {
-        console.log(err);
-        context.fail(err);
-      } else {
-        console.log(data);
-        console.log("success callback hit after update!");
-      }
-    }
-  );
+  console.log("done!");
 };
